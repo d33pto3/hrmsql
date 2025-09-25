518b655305e0c91b435829e22a4ae2057daae928
d33pto3 <rakinfrhn@gmail.com>
2025-09-26 00:11:21 +0600

    Find the category with the highest number of products.

--- pg local.session.sql
+++ pg local.session.sql
@@ -1,1 +1,15 @@
-SELECT category_id FROM products;
+SELECT category_id, count FROM 
+    (SELECT category_id, COUNT(product_id) FROM products
+    GROUP BY category_id
+    ORDER BY count desc)
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
--- practice/ANSWERS.md
+++ practice/ANSWERS.md
@@ -75,3 +75,11 @@ ORDER BY products.product_id;
 ```
 
 10.
+
+```
+SELECT category_id, count FROM
+    (SELECT category_id, COUNT(product_id) FROM products
+    GROUP BY category_id
+    ORDER BY count desc)
+LIMIT 1;
+```
