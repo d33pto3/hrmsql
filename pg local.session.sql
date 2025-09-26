d863f92cf672badf2de2992d393e1fb7883cecd8
d33pto3 <rakinfrhn@gmail.com>
2025-09-26 20:06:24 +0600

    12. Find all customers who never placed an order.

--- pg local.session.sql
+++ pg local.session.sql
@@ -1,23 +1,13 @@
-SELECT DISTINCT p.product_name FROM products AS p
-LEFT JOIN order_details AS od ON p.product_id = od.product_id
-LEFT JOIN orders AS o ON o.order_id = od.order_id
-LEFT JOIN customers AS c ON c.customer_id = o.customer_id
-WHERE c.customer_name ILIKE '%fran%'
-ORDER BY p.product_name;
+--- Using Joining
+SELECT c.customer_id, c.customer_name
+FROM customers AS c
+LEFT JOIN orders AS o 
+    ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
 
-SELECT product_name FROM products
-WHERE product_id IN (
-    SELECT product_id FROM order_details
-    WHERE order_id IN (
-        SELECT order_id FROM orders 
-        WHERE customer_id IN (
-            SELECT customer_id FROM customers
-            WHERE customer_name ILIKE '%fran%'
-        )
-    )
-)
-ORDER BY product_name;
-
-
-
+--- Using Subquery
+SELECT customer_id, customer_name FROM customers
+WHERE customer_id NOT IN (
+    SELECT DISTINCT customer_id FROM orders
+);
 
--- practice/ANSWERS.md
+++ practice/ANSWERS.md
@@ -114,3 +114,19 @@ ORDER BY p.product_name;
 ```
 
 12.
+
+```
+--- Using Joining
+SELECT c.customer_id, c.customer_name
+FROM customers AS c
+LEFT JOIN orders AS o
+    ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
+
+--- Using Subquery
+SELECT customer_id, customer_name
+FROM customers
+WHERE customer_id NOT IN (
+    SELECT DISTINCT customer_id FROM orders
+);
+```
