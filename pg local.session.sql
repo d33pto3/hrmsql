b39241cc34742ca4d6c15b36419203e34a966e9d
d33pto3 <rakinfrhn@gmail.com>
2025-09-28 01:09:56 +0600

    15. List customers who ordered half the products from category_id = 2

--- pg local.session.sql
+++ pg local.session.sql
@@ -1,3 +1,37 @@
-SELECT * FROM orders AS o
+SELECT c.customer_id, COUNT(DISTINCT p.product_id) FROM orders AS o
 LEFT JOIN order_details AS od
-    ON o.order_id = od.order_id;
+    ON o.order_id = od.order_id
+LEFT JOIN products AS p
+    ON p.product_id = od.product_id
+LEFT JOIN customers AS c
+    ON o.customer_id = c.customer_id
+WHERE p.category_id = 2
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT p.product_id) >= (
+    SELECT (COUNT(*) / 2)
+    FROM products
+    WHERE category_id = 2
+)
+ORDER BY c.customer_id;
+
+
+SELECT customer_name FROM customers
+WHERE customer_id IN (SELECT customer_id FROM orders
+WHERE order_id IN (
+SELECT order_id FROM order_details
+WHERE product_id IN(
+    SELECT product_id FROM products 
+    WHERE category_id = 2
+)
+));
+
+
+SELECT DISTINCT c.customer_name FROM orders AS o
+LEFT JOIN order_details AS od
+    ON o.order_id = od.order_id
+LEFT JOIN products AS p
+    ON p.product_id = od.product_id
+LEFT JOIN customers AS c
+    ON o.customer_id = c.customer_id
+WHERE p.category_id = 2
+ORDER BY c.customer_name;
--- practice/ANSWERS.md
+++ practice/ANSWERS.md
@@ -174,8 +174,22 @@ HAVING COUNT(od.product_id) > 20
 ORDER BY cnt;
 ```
 
-15. List customers who ordered all products from categroy_id = 2
+15. List customers who ordered half the products from categroy_id = 2
 
 ```
-
+SELECT c.customer_id, COUNT(DISTINCT p.product_id) FROM orders AS o
+LEFT JOIN order_details AS od
+    ON o.order_id = od.order_id
+LEFT JOIN products AS p
+    ON p.product_id = od.product_id
+LEFT JOIN customers AS c
+    ON o.customer_id = c.customer_id
+WHERE p.category_id = 2
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT p.product_id) >= (
+    SELECT (COUNT(*) / 2)
+    FROM products
+    WHERE category_id = 2
+)
+ORDER BY c.customer_id;
 ```
