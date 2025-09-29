acc7cca933a57060d42dbeb697e6b8acbdec26c9
d33pto3 <rakinfrhn@gmail.com>
2025-09-29 18:00:18 +0600

    20, 21, 22, 23, 24

--- pg local.session.sql
+++ pg local.session.sql
@@ -1,29 +1,35 @@
---- using EXCEPT
-SELECT * 
-FROM testproducts
-WHERE testproduct_id IN (
-    SELECT testproduct_id 
-    FROM testproducts
-    EXCEPT
-    SELECT product_id
-    FROM order_details
+-- CREATE SEQUENCE order_detail_id_seq
+--     START WITH 1
+--     INCREMENT BY 1
+--     MINVALUE 1
+--     NO MAXVALUE
+--     CACHE 1;
+
+-- CREATE SEQUENCE order_id_seq
+--     START WITH 10000
+--     INCREMENT BY 1
+--     MINVALUE 10000
+--     NO MAXVALUE
+--     CACHE 1;
+
+-- CREATE TABLE order_details_dup (
+--     order_detail_id INT PRIMARY KEY,
+--     order_id INT NOT NULL,
+--     product_id INT NOT NULL,
+--     quantity INT NOT NULL
+-- );
+
+INSERT INTO order_details_dup 
+(
+    order_detail_id,
+    order_id,
+    product_id,
+    quantity
+) VALUES (
+    nextval('order_detail_id_seq'),
+    nextval('order_id_seq'),
+    201,
+    10
 );
 
-
---- using NOT  IN
-SELECT tp.testproduct_id, tp.product_name, tp.category_id
-FROM testproducts tp
-WHERE tp.testproduct_id NOT IN (
-    SELECT DISTINCT od.product_id
-    FROM order_details od
-);
-
--- using LEFT JOIN
-SELECT 
-    tp.testproduct_id, 
-    tp.product_name, 
-    tp.category_id
-FROM testproducts tp
-LEFT JOIN order_details od
-    ON tp.testproduct_id = od.product_id
-WHERE od.product_id IS NULL;
+SELECT * FROM order_details_dup;
